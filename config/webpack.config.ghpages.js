d890416f52ff22a715bcfbd269a9329da218520e
Kvkens <kvkens@qq.com>
2019-03-01 12:50:13 +0800

    工具生成文档路径错误修复

--- config/webpack.config.ghpages.js
+++ config/webpack.config.ghpages.js
@@ -18,7 +18,7 @@ let publicPath = '/'
 if (name.startsWith('@yonyou')) {
   publicPath = name.replace('@yonyou', '')
 } else if(!name.startsWith('/')){
-  publicPath = `/${name}`
+  publicPath = `/${name}/ghpages`
 }
 
 module.exports = webpackMerge(baseConfig, {
